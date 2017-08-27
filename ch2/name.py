c0bbdf4c1d4a4fcceaf608661e80e0fcd5937d76
willtochaos <willtochaos@tuta.io>
2017-08-27 12:41:44 +0100

    Strings, methods and concatenation.

---
+++ ch2/name.py
@@ -0,0 +1,24 @@
+# Store lowercase string and then use title() method to display string
+# in titlecase.
+
+name = "ada lovelace"
+print(name.title())
+
+# Store string and then apply upper and lowercase methods.
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
+# Combining and concatenating strings.
+# Concatenate two separate variables.
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+# Display a welcome message using concatenation.
+
+print("Hello, " + full_name.title() + "!")
